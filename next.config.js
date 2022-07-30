0282804ea5401bf59c523bfee179f2ecc234ac64
Sujon Hossain <sujon.hossain758@gmail.com>
2022-07-31 00:46:18 +0600

    changing image url

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/public`],
-  },
-}
+    reactStrictMode: true,
+    images: {
+        domains: [`http://localhost:4000/`],
+    },
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
