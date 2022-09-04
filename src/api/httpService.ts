133d666cdd7c632189c977f7531e4b0711803d34
Sujon Hossain <sujon.hossain758@gmail.com>
2022-09-04 20:43:59 +0600

    update api timeout

--- src/api/httpService.ts
+++ src/api/httpService.ts
@@ -3,7 +3,7 @@ import { config } from 'src/config';
 
 const instance = axios.create({
     baseURL: config.baseUrl + '/api/',
-    // timeout: 15000,
+    timeout: 20000,
 });
 
 export default instance;
