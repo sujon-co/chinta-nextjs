9e69db999850210fa213da25bdceecb9c77e6a4b
Sujon Hossain <sujon.hossain758@gmail.com>
2022-10-05 01:07:02 +0600

    update sort format for project

--- src/pages/api/projects/index.ts
+++ src/pages/api/projects/index.ts
@@ -30,7 +30,7 @@ const uploadHandler = nextConnect<NextApiRequest, NextApiResponse>({
 })
     .get(async (req, res, next) => {
         try {
-            const projects = await Project.find({}).sort({ year: -1 });
+            const projects = await Project.find({}).sort({ year: -1, createdAt: -1 });
 
             return res.status(200).json({
                 success: true,
