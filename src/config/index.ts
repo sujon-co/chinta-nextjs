ee85357bdb9b979d9467ec0c0053409fb323fff6
Sujon Hossain <sujon.hossain758@gmail.com>
2022-10-05 12:23:45 +0600

    updated base api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,6 @@
 export const config = {
-    // jwtSecret: process.env.NEXT_PUBLIC_JWT_SECRET || '',
-    // baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || '',
-    // imageUploadUrl: process.env.NEXT_PUBLIC_API_IMAGE_UPLOAD_URL || '',
     jwtSecret: 'hi_i_am_sujon_hossain_a_developer',
-    baseUrl: 'https://chinta-nextjs.vercel.app',
+    baseUrl: 'http://184.168.120.4:3000',
     // imageUploadUrl: 'https://chinta-image-upload.herokuapp.com',
     imageUploadUrl: 'http://184.168.120.4',
     // baseUrl: 'http://localhost:3000',
