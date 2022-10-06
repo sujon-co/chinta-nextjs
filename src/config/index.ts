265e98768954718f9fc5de710cd78922e73828b9
Sujon Hossain <sujon.hossain758@gmail.com>
2022-10-06 21:57:52 +0600

    yeah server api working

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const config = {
     jwtSecret: 'hi_i_am_sujon_hossain_a_developer',
     baseUrl: 'https://chinta-nextjs.vercel.app',
-    imageUploadUrl: 'https://chinta-image-upload.herokuapp.com',
+    imageUploadUrl: 'http://chintaarchitects.com/',
 
     // baseUrl: 'http://localhost:3000',
     // imageUploadUrl: 'http://localhost:4000',
