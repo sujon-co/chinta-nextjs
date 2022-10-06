a529c9f11ee5f9b3637e2201eb3d6a93e001ca66
Sujon Hossain <sujon.hossain758@gmail.com>
2022-10-06 22:06:21 +0600

    change api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const config = {
     jwtSecret: 'hi_i_am_sujon_hossain_a_developer',
     baseUrl: 'https://chinta-nextjs.vercel.app',
-    imageUploadUrl: 'http://chintaarchitects.com/',
+    imageUploadUrl: 'https://chinta-image-upload.herokuapp.com',
 
     // baseUrl: 'http://localhost:3000',
     // imageUploadUrl: 'http://localhost:4000',
