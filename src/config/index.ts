462ff94b31aebb6610d31cf736f5fab64481a457
Sujon Hossain <sujon.hossain758@gmail.com>
2022-10-10 23:50:50 +0600

    setup main domain

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const config = {
     jwtSecret: 'hi_i_am_sujon_hossain_a_developer',
-    baseUrl: 'https://chinta-nextjs.vercel.app',
-    imageUploadUrl: 'https://chinta-image-upload.herokuapp.com',
+    baseUrl: 'https://chintaarchitects.com',
+    imageUploadUrl: 'https://api.chintaarchitects.com',
 
     // baseUrl: 'http://localhost:3000',
     // imageUploadUrl: 'http://localhost:4000',
