e113e4155b9df3bb20429f509bf532f8cf3e8ea2
Sujon Hossain <sujon.hossain758@gmail.com>
2022-10-12 13:07:27 +0600

    update baseurl

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const config = {
     jwtSecret: 'hi_i_am_sujon_hossain_a_developer',
-    baseUrl: 'https://chintaarchitects.com',
-    imageUploadUrl: 'https://chintasthapatya.com/',
+    baseUrl: 'https://chintasthapatya.com/',
+    imageUploadUrl: 'https://api.chintaarchitects.com',
 
     // baseUrl: 'http://localhost:3000',
     // imageUploadUrl: 'http://localhost:4000',
